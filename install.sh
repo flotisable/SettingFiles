54d479da097ed60702f2ad5182c7c8494b1b39c5
flotisable <s09930698@gmail.com>
2017-09-24 10:59:06 +0800

    fix bug that install script do not get argument correctly

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-#/bin/sh
+#!/bin/sh
 
 initSourceName=".emacs"
 initTargetName=".emacs"
 targetDir=${HOME}
 
-if [ -n $1 ]; then initSourceName=$1; fi
-if [ -n $2 ]; then targetDir=$2;      fi
-if [ -n $3 ]; then initSourceName=$3; fi
+if [ $# -eq 1 -a -n $1 ]; then initSourceName=$1; fi
+if [ $# -eq 2 -a -n $2 ]; then targetDir=$2;      fi
+if [ $# -eq 3 -a -n $3 ]; then initTargetName=$3; fi
 
 cp ${initSourceName} ${targetDir}/${initTargetName}
