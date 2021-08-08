ea1e1a4de72e4a4f673384c8c48423f92c1c1369
flotisable <s09930698@gmail.com>
2021-08-08 14:23:36 +0800

    add copy script which using new setting file and fix default path of mpv

---
+++ copy.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+. ./readSettings.sh "./settings.toml"
+
+targetTableName=$(mapFind "settings" "target")
+sourceTableName=$(mapFind "settings" "source")
+
+for target in $(mapKeys "$targetTableName"); do
+
+  targetFile=$(mapFind "$targetTableName" "$target")
+  sourceFile=$(mapFind "$sourceTableName" "$target")
+
+  if [ -r "$targetFile" ]; then
+
+    if [ "$target" == "playlistDir" -a -d "$targetFile" ]; then
+
+      for playlist in $(mapFind "$sourceTableName" "playlist"); do
+
+        echo "copy $playlist to $sourceFile"
+        cp $targetFile/$playlist $sourceFile
+
+      done
+      continue
+
+    fi
+
+    echo "copy $targetFile to $sourceFile"
+    cp $targetFile $sourceFile
+
+  fi
+
+done
